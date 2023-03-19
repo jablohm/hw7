a2a2a95b2970aec47369030b72b813f1a3ae3573
jablohm <77591463+jablohm@users.noreply.github.com>
2023-03-19 17:52:45 -0400

    app1

---
+++ app/app.js
@@ -0,0 +1,223 @@
+var LISTS = [
+    {
+  menu: "Coffees",
+  mainMenu: [
+// ONE
+    {
+      name: "Flight",
+      price: "$25",
+      image: "../img/home/main1.png",
+      checked: false,
+      category: hot,
+    },
+// ONE
+
+// TWO
+    {
+      name: "Iced Mocha Latte",
+      price: "$5",
+      image: "../img/home/main2.png",
+      checked: false,
+      category: cold,
+    },
+// TWO
+
+// THREE
+    { 
+        name: "Poured Pour-Over",
+        price: "$5",
+        image: "../img/home/main3.png",
+        checked: false,
+        category: hot,
+    },
+// THREE
+
+// FOUR
+    { 
+        name: "Cappuccino",
+        price: "$5",
+        image: "../img/home/main4.png",
+        checked: false,
+        category: cold,
+
+    },
+// FOUR
+
+// FIVE
+    { 
+        name: "Caramel Latte",
+        price: "$5",
+        image: "../img/home/main5.png",
+        checked: false,
+        category: hot,
+    },
+// FIVE
+
+// SIX
+    { 
+        name: "Cinnamon Latte",
+        price: "$5",
+        image: "../img/home/main6.png",
+        checked: false,
+        category: hot, 
+    },
+// SIX
+
+// SEVEN
+    { 
+        name: "Straight Black",
+        price: "$2",
+        image: "../img/home/main7.png",
+        checked: false,
+        category: hot, 
+    },
+// SEVEN
+
+// EIGHT
+    { 
+        name: "Iced Latte",
+        price: "$5",
+        image: "../img/home/main8.png",
+        checked: false,
+        category: cold, 
+    },
+// EIGHT
+
+// NINE
+    { 
+        name: "Iced Black",
+        price: "$3",
+        image: "../img/home/main9.png",
+        checked: false,
+        category: cold, 
+    },
+// NINE
+
+// TEN
+    { 
+        name: "Spiced Espresso",
+        price: "$5",
+        image: "../img/home/main10.png",
+        checked: false,
+        category: hot, 
+    },
+// TEN
+
+  ],
+},
+
+{
+    sub: "More",
+    subMenu: [
+  // ONE
+      {
+        name: "House Medium Roast",
+        price: "$10",
+        image: "../img/more/sub1.png",
+        checked: false,
+        category: whole,
+      },
+  // ONE
+  
+  // TWO
+      {
+        name: "House Blonde Roast",
+        price: "$10",  
+        image: "../img/more/sub2.png",      
+        checked: false,
+        category: ground,
+      },
+  // TWO
+  
+  // THREE
+      { 
+          name: "Pour Over I",
+          price: "$20",
+          image: "../img/more/sub3.png",
+          checked: false,
+          category: item,
+      },
+  // THREE
+  
+  // FOUR
+      { 
+          name: "Pour Over II",
+          price: "$25",
+          image: "../img/more/sub4.png",
+          checked: false,
+          category: item,
+  
+      },
+  // FOUR
+  
+  // FIVE
+      { 
+          name: "House Dark Roast",
+          price: "$10",
+          image: "../img/more/sub5.png",
+          checked: false,
+          category: whole,
+      },
+  // FIVE
+  
+  // SIX
+      { 
+          name: "House Mystery Roast",
+          price: "$10",
+          image: "../img/more/sub6.png",
+          checked: false,
+          category: whole, 
+      },
+  // SIX
+  
+  // SEVEN
+      { 
+          name: "House Espresso Shot",
+          price: "$2",
+          image: "../img/more/sub7.png",
+          checked: false,
+          category: extra, 
+      },
+  // SEVEN
+  
+  // EIGHT
+      { 
+          name: "Pour Over III",
+          price: "$30",
+          image: "../img/more/sub8.png",
+          checked: false,
+          category: item, 
+      },
+  // EIGHT
+  
+  // NINE
+      { 
+          name: "House Espresso",
+          price: "$10",
+          image: "../img/more/sub9.png",
+          checked: false,
+          category: ground, 
+      },
+  // NINE
+  
+  // TEN
+      { 
+          name: "Imported Roast",
+          price: "$15",
+          image: "../img/more/sub10.png",
+          checked: false,
+          category: ground, 
+      },
+  // TEN
+  
+    ],
+  },
+  
+
+];
+
+function initListeners() {}
+
+$(document).ready(function () {
+  initListeners();
+});
