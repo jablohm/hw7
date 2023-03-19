5026a434006859e1ab3363401d43bf12705136ad
jablohm <77591463+jablohm@users.noreply.github.com>
2023-03-19 19:41:51 -0400

    vid3

--- app/app.js
+++ app/app.js
@@ -1,115 +1,113 @@
 var LISTS = [
-{
-  name: "Coffees",
-  listItems: [
-// ONE
-    {
-      item: "Flight",
-      price: "$25",
-      image: "../img/home/main1.png",
-      checked: false,
-      category: "hot",
-    },
-// ONE
+  {
+    name: "Coffees",
+    listItems: [
+      // ONE
+      {
+        item: "Flight",
+        price: "$25",
+        image: "../img/home/main1.png",
+        checked: false,
+        category: "hot",
+      },
+      // ONE
 
-// TWO
-    {
-      item: "Iced Mocha Latte",
-      price: "$5",
-      image: "../img/home/main2.png",
-      checked: false,
-      category: "cold",
-    },
-// TWO
+      // TWO
+      {
+        item: "Iced Mocha Latte",
+        price: "$5",
+        image: "../img/home/main2.png",
+        checked: false,
+        category: "cold",
+      },
+      // TWO
 
-// THREE
-    { 
+      // THREE
+      {
         item: "Poured Pour-Over",
         price: "$5",
         image: "../img/home/main3.png",
         checked: false,
         category: "hot",
-    },
-// THREE
+      },
+      // THREE
 
-// FOUR
-    { 
+      // FOUR
+      {
         item: "Cappuccino",
         price: "$5",
         image: "../img/home/main4.png",
         checked: false,
         category: "cold",
+      },
+      // FOUR
 
-    },
-// FOUR
-
-// FIVE
-    { 
+      // FIVE
+      {
         item: "Caramel Latte",
         price: "$5",
         image: "../img/home/main5.png",
         checked: false,
         category: "hot",
-    },
-// FIVE
+      },
+      // FIVE
 
-// SIX
-    { 
+      // SIX
+      {
         item: "Cinnamon Latte",
         price: "$5",
         image: "../img/home/main6.png",
         checked: false,
-        category: "hot", 
-    },
-// SIX
+        category: "hot",
+      },
+      // SIX
 
-// SEVEN
-    { 
+      // SEVEN
+      {
         item: "Straight Black",
         price: "$2",
         image: "../img/home/main7.png",
         checked: false,
-        category: "hot", 
-    },
-// SEVEN
+        category: "hot",
+      },
+      // SEVEN
 
-// EIGHT
-    { 
+      // EIGHT
+      {
         item: "Iced Latte",
         price: "$5",
         image: "../img/home/main8.png",
         checked: false,
-        category: "cold", 
-    },
-// EIGHT
+        category: "cold",
+      },
+      // EIGHT
 
-// NINE
-    { 
+      // NINE
+      {
         item: "Iced Black",
         price: "$3",
         image: "../img/home/main9.png",
         checked: false,
-        category: "cold", 
-    },
-// NINE
+        category: "cold",
+      },
+      // NINE
 
-// TEN
-    { 
+      // TEN
+      {
         item: "Spiced Espresso",
         price: "$5",
         image: "../img/home/main10.png",
         checked: false,
-        category: "hot", 
-    },
-// TEN
+        category: "hot",
+      },
+      // TEN
+    ],
+  },
 
-  ],
-},
-
-{
+  {
     name: "More",
     listItems: [
-  // ONE
+      // ONE
       {
         item: "House Medium Roast",
         price: "$10",
@@ -117,122 +115,125 @@ var LISTS = [
         checked: false,
         category: "whole",
       },
-  // ONE
-  
-  // TWO
+      // ONE
+
+      // TWO
       {
         item: "House Blonde Roast",
-        price: "$10",  
-        image: "../img/more/sub2.png",      
+        price: "$10",
+        image: "../img/more/sub2.png",
         checked: false,
         category: "ground",
       },
-  // TWO
-  
-  // THREE
-      { 
-          item: "Pour Over I",
-          price: "$20",
-          image: "../img/more/sub3.png",
-          checked: false,
-          category: "item",
+      // TWO
+
+      // THREE
+      {
+        item: "Pour Over I",
+        price: "$20",
+        image: "../img/more/sub3.png",
+        checked: false,
+        category: "item",
       },
-  // THREE
-  
-  // FOUR
-      { 
-          item: "Pour Over II",
-          price: "$25",
-          image: "../img/more/sub4.png",
-          checked: false,
-          category: "item",
-  
+      // THREE
+
+      // FOUR
+      {
+        item: "Pour Over II",
+        price: "$25",
+        image: "../img/more/sub4.png",
+        checked: false,
+        category: "item",
       },
-  // FOUR
-  
-  // FIVE
-      { 
-          item: "House Dark Roast",
-          price: "$10",
-          image: "../img/more/sub5.png",
-          checked: false,
-          category: "whole",
+      // FOUR
+
+      // FIVE
+      {
+        item: "House Dark Roast",
+        price: "$10",
+        image: "../img/more/sub5.png",
+        checked: false,
+        category: "whole",
       },
-  // FIVE
-  
-  // SIX
-      { 
-          item: "House Mystery Roast",
-          price: "$10",
-          image: "../img/more/sub6.png",
-          checked: false,
-          category: "whole", 
+      // FIVE
+
+      // SIX
+      {
+        item: "House Mystery Roast",
+        price: "$10",
+        image: "../img/more/sub6.png",
+        checked: false,
+        category: "whole",
       },
-  // SIX
-  
-  // SEVEN
-      { 
-          item: "House Espresso Shot",
-          price: "$2",
-          image: "../img/more/sub7.png",
-          checked: false,
-          category: "", 
+      // SIX
+
+      // SEVEN
+      {
+        item: "House Espresso Shot",
+        price: "$2",
+        image: "../img/more/sub7.png",
+        checked: false,
+        category: "",
       },
-  // SEVEN
-  
-  // EIGHT
-      { 
-          item: "Pour Over III",
-          price: "$30",
-          image: "../img/more/sub8.png",
-          checked: false,
-          category: "item", 
+      // SEVEN
+
+      // EIGHT
+      {
+        item: "Pour Over III",
+        price: "$30",
+        image: "../img/more/sub8.png",
+        checked: false,
+        category: "item",
       },
-  // EIGHT
-  
-  // NINE
-      { 
-          item: "House Espresso",
-          price: "$10",
-          image: "../img/more/sub9.png",
-          checked: false,
-          category: "ground", 
+      // EIGHT
+
+      // NINE
+      {
+        item: "House Espresso",
+        price: "$10",
+        image: "../img/more/sub9.png",
+        checked: false,
+        category: "ground",
       },
-  // NINE
-  
-  // TEN
-      { 
-          item: "Imported Roast",
-          price: "$15",
-          image: "../img/more/sub10.png",
-          checked: false,
-          category: "ground", 
+      // NINE
+
+      // TEN
+      {
+        item: "Imported Roast",
+        price: "$15",
+        image: "../img/more/sub10.png",
+        checked: false,
+        category: "ground",
       },
-  // TEN
-  
+      // TEN
     ],
   },
-  
-
 ];
 
-function loadListItems(itemIndex){
-    console.log(itemIndex);
+function loadListItems(itemIndex) {
+  let listString = "<ul>";
+
+  $.each(LISTS[itemIndex].listItems, function (index, listItems) {
+    listString += `<li id="${index}">${listItems.item}</li>`;
+  });
+  listString += "</ul>";
+
+  $("#app").html(listString);
 }
 
-function loadData(){
-    let listString = "<ul>";
-    $.each(LISTS, function(index, list){
-        listString += `<li id="${index}" onclick="loadListItems(${index})">${list.name}</li>`;
-    });
-    listString += "</ul>";
+function loadData() {
+  let listString = "<ul>";
+  $.each(LISTS, function (index, list) {
+    listString += `<li id="${index}" onclick="loadListItems(${index})">${list.name}</li>`;
+  });
+  listString += "</ul>";
 
-    $("#app").html(listString);
+  $("#app").html(listString);
 }
 
 function initListeners() {}
 
 $(document).ready(function () {
-    // loadData();
+  // loadData();
   initListeners();
 });
